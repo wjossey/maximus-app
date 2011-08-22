c34f044e7ec64a4729f6c7d01b338c261d41082b
Weston Jossey <weston.jossey@gmail.com>
2011-08-22 13:43:30 -0400

    Updating names.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ PartyGirl::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'party-girl.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'maximus-app.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-PartyGirl::Application.config.session_store :cookie_store, :key => '_party-girl_session'
+PartyGirl::Application.config.session_store :cookie_store, :key => '_maximus_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
